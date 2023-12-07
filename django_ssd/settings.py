42067dd935298d988f3f1092c583d638ae46bf50
GiovanniCrudo00 <crugio23@gmail.com>
2023-12-07 18:11:54 +0100

    apps and basic configuration

--- django_ssd/settings.py
+++ django_ssd/settings.py
@@ -37,17 +37,30 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",  # dev server
+    # add front-end server
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
 
 ROOT_URLCONF = 'django_ssd.urls'
 
--- django_ssd/urls.py
+++ django_ssd/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+API_TITLE = 'Your Secure Weather API'
+API_DESCRIPTION = 'A web API for weather forecasting'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-LITroTHeraWaRi/', admin.site.urls),
+    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+    path('schema/', get_schema_view(title=API_TITLE)),
 ]
